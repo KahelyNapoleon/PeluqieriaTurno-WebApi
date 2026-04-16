064bef25c68e0da7398cecbd65bcc7cf79fe9091
Aguirre Gabriel <AguirreGabrielNahuel@hotmail.com>
2026-04-16 02:39:33 -0300

    Carpeta-FueraDeLugar

--- LinqYLambdaExpression/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿List<int> list = new List<int>() { 20, 1, 4, 8, 9, 44 };
-// Process each argument with code statements
-var evenNumbers = list.FindAll((i) =>
-{
-    Console.WriteLine("Value of i is: {0}", i);
-    return (i % 2) == 0;
-});
